7bfdf8636614c435211bd6be5a9aab114e3d0320
Marwan Amr <marwanesawy2005@gmail.com>
2026-09-21 12:42:55 +0300

    Implement homework review request flow and fix ParentDashboardUI status bar padding in Android

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"a804b261645ef8c13eb3d5c44a5c2fb0340c55
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "582872264" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1398988246" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
